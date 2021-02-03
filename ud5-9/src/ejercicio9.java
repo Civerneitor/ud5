07d55443abef4ce4933080e4e0da1ba65a16f921
Sergio <sergio_castilla_13@hotmail.com>
2021-02-03 16:22:38 +0100

    ejercicio9

---
+++ ud5-9/src/ejercicio9.java
@@ -0,0 +1,12 @@
+
+public class ejercicio9 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int n=1; n<=100;n++) {
+			if(n%2==0 && n%3==0) {
+			System.out.println(n);
+			}
+		}
+		
+	}
+}
