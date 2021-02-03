8c71d78314746536a38f09763d63ca455128062c
Sergio <sergio_castilla_13@hotmail.com>
2021-02-03 16:17:43 +0100

    ejercicio7

---
+++ ud5-7/src/ejercicio7.java
@@ -0,0 +1,14 @@
+
+public class ejercicio7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		while (n<=100) {
+			System.out.println(n);
+			n++;
+		}
+		
+	}
+
+}
