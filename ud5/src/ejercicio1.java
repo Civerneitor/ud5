d98b868e83b1fce284f9376b9929b181c5ac1fd7
Sergio <sergio_castilla_13@hotmail.com>
2021-02-03 15:25:07 +0100

    ejercicio1

---
+++ ud5/src/ejercicio1.java
@@ -0,0 +1,17 @@
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b = 4;
+		if(a>b) {
+			System.out.println("a es mayor que b");
+		}else if(b>a) {
+			System.out.println("b es mayor que a");
+		}else {
+			System.out.println("a es igual que b");
+		}
+	}
+
+}
