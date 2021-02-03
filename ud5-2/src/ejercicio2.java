47e2797441361514b10c5e606d76fe5be5ed9377
Sergio <sergio_castilla_13@hotmail.com>
2021-02-03 15:29:06 +0100

    ejercicio2

---
+++ ud5-2/src/ejercicio2.java
@@ -0,0 +1,10 @@
+
+public class ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Sergio";
+		System.out.println("Bienvenido "+name);
+	}
+
+}
