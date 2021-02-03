161732527081a75b17f907ae74d912dc694b17bd
Sergio <sergio_castilla_13@hotmail.com>
2021-02-03 16:20:14 +0100

    ejercicio8

---
+++ ud5-8/src/ejercicio8.java
@@ -0,0 +1,13 @@
+
+public class ejercicio8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int n=1; n<=100;n++) {
+			System.out.println(n);
+			
+		}
+		
+	}
+
+}
